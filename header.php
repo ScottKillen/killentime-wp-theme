7bc63b7c0fc58a81b4741894b350e3e4e783caba
Scott Killen <scott@scottkillen.com>
2023-08-12 09:48:14 -0400

    Add initial logo size

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<style>.logo{height: 3.75rem}.site-title {display: none}</style>
 	<?php wp_head(); ?>
 </head>
 
