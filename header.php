f618fd31d4705c3e235f198d3933bfb8007b8a44
Scott Killen <scott@scottkillen.com>
2023-08-16 00:46:14 -0400

    Change blog name and logo

--- header.php
+++ header.php
@@ -52,9 +52,9 @@
 	<header id="header">
 		<div id="head" class="text-center py-5">
 			<h1 id="logo" class="text-center">
-				<img class="rounded-circle d-block overflow-hidden border border-secondary mx-auto my-0" src="/wp-content/themes/scottkillen/images/scott.jpg" alt="me" />
-				<span class="title fs-1">Scott Killen</span>
-				<span class="tagline text-body-secondary d-block my-2 fs-6">Preacher, Acountant, Regular Guy<br /><a class="text-body-tertiary text-decoration-none" href>scott@scottkillen.com</a></span>
+				<img class="rounded-circle d-block overflow-hidden border border-secondary mx-auto my-0" src="<?php echo get_template_directory_uri() . '/images/killentime.png'; ?>" alt="Killentime logo" />
+				<span class="title fs-1">Killentime</span>
+				<span class="tagline text-body-secondary d-block my-2 fs-6">Stuff Scott Killen says...<br /><a class="text-body-tertiary text-decoration-none" href>(for whatever it's worth)</a></span>
 			</h1><!-- #logo -->
 		</div><!-- #head -->
 	</header> <!-- #header -->
