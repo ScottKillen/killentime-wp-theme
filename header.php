d0548b153f77d51c2c997f452c7a2b1b263de31f
Scott Killen <scott@scottkillen.com>
2023-08-17 08:16:50 -0400

    Add Header Animation

--- header.php
+++ header.php
@@ -13,12 +13,13 @@
 <!doctype html>
 <html <?php language_attributes(); ?> data-bs-theme="light">
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@500&display=swap" rel="stylesheet" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet" />
 	<?php wp_head(); ?>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/515e1e91da.js" crossorigin="anonymous"></script>
--- js/header.js
+++ js/header.js
@@ -2,7 +2,9 @@ window.addEventListener('scroll', function (e) {
   var brand = document.getElementById('brand');
   if (document.getElementsByTagName('html')[0].scrollTop > 200) {
     brand.classList.remove('invisible');
+    brand.classList.add('animate__fadeIn', 'animate__animated');
   } else {
     brand.classList.add('invisible');
+    brand.classList.remove('animate__fadeIn', 'animate__animated');
   }
 });
