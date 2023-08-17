fd1d41c4adedf9d27741b0571f5f66ce00eb16a6
Scott Killen <scott@scottkillen.com>
2023-08-17 08:02:41 -0400

    Change header

--- header.php
+++ header.php
@@ -64,49 +64,53 @@
 	<?php endif; ?>
 	<nav class="py-2 mb-4 navbar navbar-expand-lg border-bottom border-top sticky-top shadow-sm" id="site-navigation" class="main-navigation">
 		<div class="container-fluid">
-			<a class="navbar-brand d-none" id="brand" hrf="/">Killentime</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar1" aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<?php
-			wp_nav_menu(
-				array(
-					'menu_class' => 'nav nav-pills',
-					'menu_id' => 'primary-menu',
-					'container_class' => 'collapse navbar-collapse',
-					'container_id' => 'navbarNavDropdown',
-					'theme_location' => 'menu-1',
-					'add_li_class' => 'nav-item',
-					'add_link_class' => 'nav-link'
-				)
-			); ?>
-			<ul class="navbar-nav flex-row-flex-wrap ms-md-auto">
-				<li class="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-					<div class="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-					<hr class="d-lg-none my-2 text-white-50">
-				</li>
-				<li class="nav-item dropdown">
-					<button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (auto)">
-						<svg class="bi my-1 theme-icon-active"><use href="#circle-half"></use></svg>
-						<span class="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
-					</button>
-					<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
-						<li>
-							<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-								<svg class="bi me-2 opacity-50 theme-icon"><use href="#sun-fill"></use></svg>Light<svg class="bi ms-auto d-none"><use href="#check2"></use></svg>
-							</button>
+			<div class="collapse navbar-collapse d-lg-flex" id="navbar1">
+				<a class="navbar-brand col-lg-3 me-0 invisible" id="brand" hrf="/">Killentime</a>
+				<?php
+				wp_nav_menu(
+					array(
+						'menu_class' => 'navbar-nav col-lg-6 justify-content-center nav-pills',
+						'menu_id' => 'primary-menu',
+						'container' => false,
+						'theme_location' => 'menu-1',
+						'add_li_class' => 'nav-item',
+						'add_link_class' => 'nav-link'
+					)
+				); ?>
+				<div class="d-lg-flex col-lg-3 justify-content-end">
+					<ul class="navbar-nav flex-row-flex-wrap ms-md-auto">
+						<li class="nav-item py-2 py-lg-1 col-12 col-lg-auto">
+							<div class="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
+							<hr class="d-lg-none my-2 text-white-50">
 						</li>
-						<li>
-							<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-								<svg class="bi me-2 opacity-50 theme-icon"><use href="#moon-stars-fill"></use></svg>Dark<svg class="bi ms-auto d-none"><use href="#check2"></use></svg>
+						<li class="nav-item dropdown">
+							<button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (auto)">
+								<svg class="bi my-1 theme-icon-active"><use href="#circle-half"></use></svg>
+								<span class="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
 							</button>
-						</li>
-						<li>
-							<button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-								<svg class="bi me-2 opacity-50 theme-icon"><use href="#circle-half"></use></svg>Auto<svg class="bi ms-auto d-none"><use href="#check2"></use></svg>
-							</button>
+							<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
+								<li>
+									<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
+										<svg class="bi me-2 opacity-50 theme-icon"><use href="#sun-fill"></use></svg>Light<svg class="bi ms-auto d-none"><use href="#check2"></use></svg>
+									</button>
+								</li>
+								<li>
+									<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
+										<svg class="bi me-2 opacity-50 theme-icon"><use href="#moon-stars-fill"></use></svg>Dark<svg class="bi ms-auto d-none"><use href="#check2"></use></svg>
+									</button>
+								</li>
+								<li>
+									<button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
+										<svg class="bi me-2 opacity-50 theme-icon"><use href="#circle-half"></use></svg>Auto<svg class="bi ms-auto d-none"><use href="#check2"></use></svg>
+									</button>
+								</li>
+							</ul>
 						</li>
 					</ul>
-				</li>
-			</ul>
-</nav><!-- #site-navigation -->
+			</div>
+			<div> <!-- #navbar1 -->
+		</div>
+	</nav><!-- #site-navigation -->
--- js/header.js
+++ js/header.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function (e) {
   var brand = document.getElementById('brand');
   if (document.getElementsByTagName('html')[0].scrollTop > 200) {
-    brand.classList.remove('d-none');
+    brand.classList.remove('invisible');
   } else {
-    brand.classList.add('d-none');
+    brand.classList.add('invisible');
   }
 });
