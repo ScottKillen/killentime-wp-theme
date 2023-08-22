45b2990c6eb851d19ccc63e89c35bc9ac09b43e0
Scott Killen <scott@scottkillen.com>
2023-08-21 20:22:53 -0400

    Fix footer

--- footer.php
+++ footer.php
@@ -12,16 +12,15 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="d-flex flex-column flex-sm-row justify-content-between pt-2 my-4 border-top">
-      <p>© <?php echo date('Y'); ?> Scott Killen. All rights reserved.<a href="/" class="mb-3 ms-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-				<svg class="bi link-body-emphasis" width="1.75em" height="1.75em"><use xlink:href="#sk-logo" fill="currentColor"></use></svg>
-			</a></p>
-			<ul class="list-unstyled d-flex">
-				<li class="ms-3"><a class="link-body-emphasis" href="https://ttocs.io/@scott" rel="me"><i class="fa-brands fa-mastodon"></i></a></li>
-				<li class="ms-3"><a class="link-body-emphasis" href="https://github.com/ScottKillen" rel="me"><i class="fa-brands fa-github"></i></a></li>
-				<li class="ms-3"><a class="link-body-emphasis" href="https://open.spotify.com/user/sdkillen" rel="me"><i class="fa-brands fa-spotify"></i></a></li>
-				<li class="ms-3"><a class="link-body-emphasis" href="https://www.flickr.com/photos/scottkillen" rel="me"><i class="fa-brands fa-flickr"></i></a></li>
-				<li class="ms-3"><a class="link-body-emphasis" href="https://www.instagram.com/scottdkillen/" rel="me"><i class="fa-brands fa-instagram"></i></a></li>
+		<div class="d-flex flex-wrap justify-content-between align-items-center pt-2 my-4 border-top">
+      <p class="col-md-4 mb-0 text-body-secondary font-accent">© <?php echo date('Y'); ?> Scott Killen. All rights reserved.</p>
+			<a href="/" class="font-title col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-decoration-none link-body-emphasis">Killentime</a>
+			<ul class="nav col-md-4 justify-content-end">
+				<li class="nav-item"><a class="nav-link px-2 text-body-secondary" href="https://ttocs.io/@scott" rel="me"><i class="fa-brands fa-mastodon"></i></a></li>
+				<li class="nav-item"><a class="nav-link px-2 text-body-secondary" href="https://github.com/ScottKillen" rel="me"><i class="fa-brands fa-github"></i></a></li>
+				<li class="nav-item"><a class="nav-link px-2 text-body-secondary" href="https://open.spotify.com/user/sdkillen" rel="me"><i class="fa-brands fa-spotify"></i></a></li>
+				<li class="nav-item"><a class="nav-link px-2 text-body-secondary" href="https://www.flickr.com/photos/scottkillen" rel="me"><i class="fa-brands fa-flickr"></i></a></li>
+				<li class="nav-item"><a class="nav-link px-2 text-body-secondary" href="https://www.instagram.com/scottdkillen/" rel="me"><i class="fa-brands fa-instagram"></i></a></li>
 			</ul>
 		</div>
 	</footer>
