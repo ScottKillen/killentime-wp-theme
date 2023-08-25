226b53036ab6651a343a156b4c22643b49a6a4af
Scott Killen <scott@scottkillen.com>
2023-08-25 15:58:14 -0400

    post thumnails only in post

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -9,43 +10,41 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php KT_post_class(); ?> >
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title display-5 mb-1"><a class="link-body-emphasis text-decoration-none" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+<article id="post-<?php the_ID(); ?>" <?php KT_post_class(); ?>>
+	<?php
+	if (is_singular()) :
+		the_title('<h1 class="entry-title">', '</h1>');
+	else :
+		the_title('<h2 class="entry-title display-5 mb-1"><a class="link-body-emphasis text-decoration-none" href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+	endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<p class="blog-post-meta text-secondary font-accent">
-				<?php
-				killentime_posted_by();
-				if (!is_singular()) {
-					killentime_posted_in();
-				}
-				killentime_posted_on(); ?>
-			</p>
-		<?php endif; ?>
+	if ('post' === get_post_type()) :
+	?>
+		<p class="blog-post-meta text-secondary font-accent">
+			<?php
+			killentime_posted_by();
+			if (!is_singular()) {
+				killentime_posted_in();
+			}
+			killentime_posted_on(); ?>
+		</p>
+	<?php endif;
 
-	<?php killentime_post_thumbnail(); ?>
+	if (is_singular()) {
+		killentime_post_thumbnail();
+		the_content();
+	} else {
+		KT_home_excerpt($post);
+	}
 
-		<?php
-		if (is_singular()) {
-			the_content();
-		} else {
-			KT_home_excerpt($post);
-		}
-
-/*		wp_link_pages(
+	/*		wp_link_pages(
  *			array(
  *				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'killentime' ),
  *				'after'  => '</div>',
  *			)
  *		);
  */
-		?>
+	?>
 
 	<footer class="entry-footer">
 		<?php killentime_entry_footer(); ?>
