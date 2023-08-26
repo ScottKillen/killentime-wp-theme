f482ae308305b58348df4285be5936d9c5ba066a
Scott Killen <scott@scottkillen.com>
2023-08-26 14:54:06 -0400

    Add related posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -72,22 +72,51 @@ if (!function_exists('killentime_entry_footer')) :
 	 */
 	function killentime_entry_footer()
 	{ ?>
-		<div class="py-3 my-2 border-top">
-			<div class="shadow rounded bg-primary-subtle border p-2">
-				<?php
-				// Hide category and tag text for pages.
-				if ('post' === get_post_type()) {
+		<div class="shadow rounded bg-primary-subtle border p-2 mt-5">
+			<?php
+			// Hide category and tag text for pages.
+			if ('post' === get_post_type()) {
 
-					$before_tag = '<span class="badge text-bg-primary rounded-pill">';
-					$after_tag = '</span>';
-					$tags_title = '<p class="h5 fst-italic"><svg class="bi"><title>Tags</title><use xlink:href="#hashtag"/></svg> Tags</p>';
-					the_tags(
-						$tags_title . '<div class="d-flex gap-2">' . $before_tag,
-						$after_tag . $before_tag,
-						$after_tag . '</div>'
-					);
-				} ?>
-			</div>
+				$before_tag = '<span class="badge text-bg-primary rounded-pill">';
+				$after_tag = '</span>';
+				$tags_title = '<p class="h5 fst-italic"><svg class="bi"><title>Tags</title><use xlink:href="#hashtag"/></svg> Tags</p>';
+				the_tags(
+					$tags_title . '<div class="d-flex gap-2">' . $before_tag,
+					$after_tag . $before_tag,
+					$after_tag . '</div>'
+				);
+			}
+			global $post;
+			$orig_post = $post;
+			$tags = wp_get_post_tags($post->ID);
+			if ($tags) {
+				$tag_ids = array();
+				foreach ($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+				$args = array(
+					'tag__in' => $tag_ids,
+					'post__not_in' => array($post->ID),
+					'posts_per_page' => 3, // Number of related posts that will be shown.
+					'ignore_sticky_posts' => 1
+				);
+				$my_query = new wp_query($args);
+				if ($my_query->have_posts()) {
+					echo '<div id="relatedposts"><p class= "mt-4 h5 fst-italic"><svg class="bi"><title>Related Posts</title><use xlink:href="#posts"/></svg> Related Posts</h3>';
+					echo '<div class="row">';
+					while ($my_query->have_posts()) {
+						$my_query->the_post(); ?>
+						<div class="col">
+							<div class="relatedcontent">
+								<p class="font-title mb-0"><a href="<?php the_permalink() ?>" rel="bookmark" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover link-opacity-75 link-opacity-100-hover" title="<?php the_title(); ?>"><?php the_title(); ?></a></p>
+								<p class="text-secondary"><?php the_time('M j, Y') ?> <?php killentime_posted_in() ?></p>
+
+							</div>
+						</div>
+			<?php }
+					echo '</div></div>';
+				}
+			}
+			$post = $orig_post;
+			wp_reset_query(); ?>
 		</div>
 		<?php
 	}
