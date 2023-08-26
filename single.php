7f8fb0d7692a270804411409b3dedfc6feda108d
Scott Killen <scott@scottkillen.com>
2023-08-26 16:58:50 -0400

    REmove navigation links to previous and next posts

--- single.php
+++ single.php
@@ -24,18 +24,8 @@ get_header();
 			}
 		}
 
-		get_template_part('template-parts/content', get_post_type()); ?>
+		get_template_part('template-parts/content', get_post_type());
 
-		<div class="clearfix">
-			<?php
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'killentime') . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'killentime') . '</span> <span class="nav-title">%title</span>',
-				)
-			); ?>
-		</div>
-	<?php
 		// If comments are open or we have at least one comment, load up the comment template.
 		if (comments_open() || get_comments_number()) :
 			comments_template();
