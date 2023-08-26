f1db75323b06d2b566173c79e70f5577abc00cc4
Scott Killen <scott@scottkillen.com>
2023-08-26 13:49:10 -0400

    move border to extrernal

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Custom template tags for this theme
  *
@@ -7,20 +8,21 @@
  * @package Scott_Killen
  */
 
-if ( ! function_exists( 'killentime_posted_on' ) ) :
+if (!function_exists('killentime_posted_on')) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
-	function killentime_posted_on() {
+	function killentime_posted_on()
+	{
 		$time_string = ' <svg class="bi"><title>published</title><use xlink:href="#clock"/></svg> %1$s';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		if (get_the_time('U') !== get_the_modified_time('U')) {
 			$time_string .= ' <span class="fst-italic post-update-meta text-decoration-underline">(updated %2$s)</span>';
 		}
 
 		$time_string = sprintf(
 			$time_string,
-			esc_html( get_the_date() ),
-			esc_html( get_the_modified_date() )
+			esc_html(get_the_date()),
+			esc_html(get_the_modified_date())
 		);
 
 		echo $time_string; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -28,15 +30,16 @@ if ( ! function_exists( 'killentime_posted_on' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'killentime_posted_by' ) ) :
+if (!function_exists('killentime_posted_by')) :
 	/**
 	 * Prints HTML with meta information for the current author.
 	 */
-	function killentime_posted_by() {
+	function killentime_posted_by()
+	{
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( 'by %s', 'post author', 'killentime' ),
-			'<span class="author vcard me-1"><a class="link-secondary text-decoration-none fw-bold link-secondary-emphasis" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			esc_html_x('by %s', 'post author', 'killentime'),
+			'<span class="author vcard me-1"><a class="link-secondary text-decoration-none fw-bold link-secondary-emphasis" href="' . esc_url(get_author_posts_url(get_the_author_meta('ID'))) . '">' . esc_html(get_the_author()) . '</a></span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -44,11 +47,12 @@ if ( ! function_exists( 'killentime_posted_by' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'killentime_posted_in' ) ) :
+if (!function_exists('killentime_posted_in')) :
 	/**
 	 * Prints HTML with meta information for the post's category
 	 */
-	function killentime_posted_in() {
+	function killentime_posted_in()
+	{
 		$categories = get_the_category();
 		if (empty($categories)) {
 			return;
@@ -58,45 +62,52 @@ if ( ! function_exists( 'killentime_posted_in' ) ) :
 			return;
 		}
 
-		echo 'in <a class="me-1 link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="'.esc_url(get_category_link($categories[0]->term_id)).'">'.esc_html($categories[0]->name)."</a>";
+		echo 'in <a class="me-1 link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="' . esc_url(get_category_link($categories[0]->term_id)) . '">' . esc_html($categories[0]->name) . "</a>";
 	}
 endif;
 
-if ( ! function_exists( 'killentime_entry_footer' ) ) :
+if (!function_exists('killentime_entry_footer')) :
 	/**
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
-	function killentime_entry_footer() {
+	function killentime_entry_footer()
+	{ ?>
+		<div class="py-3 my-2 border-top">
+			<div class="shadow rounded bg-primary-subtle border p-2">
+				<?php
+				// Hide category and tag text for pages.
+				if ('post' === get_post_type()) {
 
-		// Hide category and tag text for pages.
-		if ('post' === get_post_type()) {
-
-			$before_tag = '<span class="badge text-bg-primary rounded-pill">';
-			$after_tag = '</span>';
-			$tags_title = '<div class="py-3 my-2 border-top"><div class="shadow rounded bg-primary-subtle border p-2"><p class="h5 fst-italic"><svg class="bi"><title>Tags</title><use xlink:href="#hashtag"/></svg> Tags</p>';
-			the_tags(
-				$tags_title . '<div class="d-flex gap-2">' . $before_tag,
-				$after_tag . $before_tag,
-				$after_tag . '</div></div></div>'
-			);
-		}
+					$before_tag = '<span class="badge text-bg-primary rounded-pill">';
+					$after_tag = '</span>';
+					$tags_title = '<p class="h5 fst-italic"><svg class="bi"><title>Tags</title><use xlink:href="#hashtag"/></svg> Tags</p>';
+					the_tags(
+						$tags_title . '<div class="d-flex gap-2">' . $before_tag,
+						$after_tag . $before_tag,
+						$after_tag . '</div>'
+					);
+				} ?>
+			</div>
+		</div>
+		<?php
 	}
 endif;
 
-if ( ! function_exists( 'killentime_post_thumbnail' ) ) :
+if (!function_exists('killentime_post_thumbnail')) :
 	/**
 	 * Displays an optional post thumbnail.
 	 *
 	 * Wraps the post thumbnail in an anchor element on index views, or a div
 	 * element when on single views.
 	 */
-	function killentime_post_thumbnail() {
-		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+	function killentime_post_thumbnail()
+	{
+		if (post_password_required() || is_attachment() || !has_post_thumbnail()) {
 			return;
 		}
 
-		if ( is_singular() ) :
-			?>
+		if (is_singular()) :
+		?>
 
 			<div class="post-thumbnail float-end ms-3 mb-3 shadow rounded overflow-hidden">
 				<?php the_post_thumbnail(); ?>
@@ -106,31 +117,32 @@ if ( ! function_exists( 'killentime_post_thumbnail' ) ) :
 
 			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
 				<?php
-					the_post_thumbnail(
-						'post-thumbnail',
-						array(
-							'alt' => the_title_attribute(
-								array(
-									'echo' => false,
-								)
-							),
-						)
-					);
+				the_post_thumbnail(
+					'post-thumbnail',
+					array(
+						'alt' => the_title_attribute(
+							array(
+								'echo' => false,
+							)
+						),
+					)
+				);
 				?>
 			</a>
 
-			<?php
+<?php
 		endif; // End is_singular().
 	}
 endif;
 
-if ( ! function_exists( 'wp_body_open' ) ) :
+if (!function_exists('wp_body_open')) :
 	/**
 	 * Shim for sites older than 5.2.
 	 *
 	 * @link https://core.trac.wordpress.org/ticket/12563
 	 */
-	function wp_body_open() {
-		do_action( 'wp_body_open' );
+	function wp_body_open()
+	{
+		do_action('wp_body_open');
 	}
 endif;
