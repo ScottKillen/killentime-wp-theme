019c4ab4b2a72f804aa026b3566c8f47cc7ec2e8
Scott Killen <scott@scottkillen.com>
2023-09-01 17:59:56 -0400

    Fix page template

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -14,17 +15,18 @@
 
 get_header();
 ?>
+<div class="row">
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main col-md-8">
 
 		<?php
-		while ( have_posts() ) :
+		while (have_posts()) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part('template-parts/content', 'page');
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
+			if (comments_open() || get_comments_number()) :
 				comments_template();
 			endif;
 
@@ -33,6 +35,9 @@ get_header();
 
 	</main><!-- #main -->
 
+	<?php
+	get_sidebar();
+	?>
+</div>
 <?php
-get_sidebar();
 get_footer();
