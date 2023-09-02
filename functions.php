98d5e44ffdd34c52546adbc532fe0f2101730ef2
Scott Killen <scott@scottkillen.com>
2023-09-02 16:25:35 -0400

    Remove unneccessary customization

--- functions.php
+++ functions.php
@@ -139,11 +139,6 @@ function killentime_scripts()
 add_action('wp_enqueue_scripts', 'killentime_scripts');
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
