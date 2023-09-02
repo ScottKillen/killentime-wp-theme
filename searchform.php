9f9cac93445a3c38d1373a7448ee74db2136f59b
Scott Killen <scott@scottkillen.com>
2023-09-02 15:11:45 -0400

    Optimize search form

--- searchform.php
+++ searchform.php
@@ -2,13 +2,16 @@
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')) ?>">
   <div class="input-group my-3">
-    <span class=" input-group-text" id="basic-addon3">
+    <!-- Search icon -->
+    <span class="input-group-text" id="basic-addon3">
       <svg class="bi">
         <use xlink:href="#fa-magnifying-glass" />
       </svg>
     </span>
+    <!-- Search input field -->
     <input type="search" class="search-field form-control" id="search" value="<?php echo get_search_query() ?>" name="s">
   </div>
+  <!-- Submit button -->
   <div>
     <button type="submit" class="search-submit btn btn-primary">Search</button>
   </div>
