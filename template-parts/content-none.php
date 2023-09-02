87cd1f3c1db0c34724ba30bc54c1a5102ddf580b
Scott Killen <scott@scottkillen.com>
2023-09-02 15:25:56 -0400

    Finish content-none template

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying a message that posts cannot be found
  *
@@ -7,45 +8,58 @@
  * @package Scott_Killen
  */
 
+$search_query = get_search_query();
+$categories_title = esc_html__('Most Used Categories', 'killentime');
+
+// Function to apply custom CSS classes to tag cloud links
+function custom_tag_cloud_links($tag_cloud)
+{
+	return str_replace(
+		'"tag-cloud-link',
+		'"pe-2 link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover tag-cloud-link',
+		$tag_cloud
+	);
+}
 ?>
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'killentime' ); ?></h1>
+		<img alt="Sad looking guy" class="d-block my-2 mx-auto rounded-circle border border-secondary" height="384" widget="384" src="<?php echo esc_url(get_theme_file_uri('/images/404.png')); ?>">
+		<h1 class="fst-italic page-title text-center"><?php echo esc_html__('Oops! We couldn&rsquo;t find "', 'killentime') . $search_query . '"'; ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+		<p class="text-info text-center"><?php echo esc_html__('Try the links on this page to get your bearings.', 'killentime'); ?></p>
 
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'killentime' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
+		<?php get_search_form(); ?>
 
-		elseif ( is_search() ) :
-			?>
+		<div class="d-flex gap-3">
+			<div class="widget widget_categories my-4 col-6">
+				<p class="h2 fst-italic widget-title"><?php echo $categories_title; ?></p>
+				<div class="list-group">
+					<?php
+					wp_list_categories(
+						array(
+							'orderby'    => 'count',
+							'order'      => 'DESC',
+							'show_count' => 1,
+							'title_li'   => '',
+							'number'     => 10,
+						)
+					);
+					?>
+				</div>
+			</div><!-- .widget -->
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'killentime' ); ?></p>
-			<?php
-			get_search_form();
-
-		else :
-			?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'killentime' ); ?></p>
-			<?php
-			get_search_form();
-
-		endif;
-		?>
+			<div class="widget widget_categories my-4 col-6">
+				<p class="h2 fst-italic widget-title">Tags</p>
+				<?php
+				ob_start();
+				wp_tag_cloud();
+				$tag_cloud = ob_get_clean();
+				echo custom_tag_cloud_links($tag_cloud);
+				?>
+			</div>
+		</div>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
