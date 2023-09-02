35d0e1579b64457d2955004cef01b63baf4eb208
Scott Killen <scott@scottkillen.com>
2023-09-02 15:15:36 -0400

    Optimize singular template

--- single.php
+++ single.php
@@ -18,10 +18,8 @@ get_header();
 		the_post();
 
 		$categories = get_the_category();
-		if (!empty($categories)) {
-			if ($categories[0]->name != "Uncategorized") {
-				echo '<a class="badge text-bg-primary text-decoration-none" href="' . esc_url(get_category_link($categories[0]->term_id)) . '">' . esc_html($categories[0]->name) . "</a>";
-			}
+		if (!empty($categories) && $categories[0]->name !== "Uncategorized") {
+			echo '<a class="badge text-bg-primary text-decoration-none" href="' . esc_url(get_category_link($categories[0]->term_id)) . '">' . esc_html($categories[0]->name) . "</a>";
 		}
 
 		get_template_part('template-parts/content', get_post_type());
