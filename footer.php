ed3dfb980aac0c5d90fde0d556f8cf3e145909f2
Scott Killen <scott@scottkillen.com>
2023-09-02 15:10:09 -0400

    Fix home link in footer.

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 <footer id="colophon" class="site-footer">
 	<div class="d-flex flex-wrap justify-content-between align-items-center pt-2 my-4 border-top">
 		<p class="col-md-4 mb-0 text-body-secondary font-accent">© <?php echo date('Y'); ?> Scott Killen. All rights reserved.</p>
-		<a href="/" class="font-title col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-decoration-none link-body-emphasis">
+		<a href="<?php echo esc_url(home_url('/')) ?>" class="font-title col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-decoration-none link-body-emphasis">
 			<?php bloginfo('name'); ?>
 		</a>
 		<ul class="nav col-md-4 justify-content-end">
