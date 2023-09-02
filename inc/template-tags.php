747631b9c763a635dea38456dfacfb5ccd155b5c
Scott Killen <scott@scottkillen.com>
2023-09-01 22:03:57 -0400

    Add edit button to posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -72,6 +72,30 @@ if (!function_exists('killentime_entry_footer')) :
 	 */
 	function killentime_entry_footer()
 	{
+		if (get_edit_post_link()) : ?>
+			<footer class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__('Edit <span class="screen-reader-text">%s</span>', 'killentime'),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post(get_the_title())
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</footer><!-- .entry-footer -->
+		<?php
+		endif;
+
 		global $post;
 		$orig_post = $post;
 		$tags = wp_get_post_tags($post->ID);
