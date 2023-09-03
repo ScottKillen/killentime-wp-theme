b4bf13808201bd03dc819ae3a9fcae524280153a
Scott Killen <scott@scottkillen.com>
2023-09-03 17:32:19 -0400

    Optimized nav search

--- template-parts/header/nav-search.php
+++ template-parts/header/nav-search.php
@@ -10,9 +10,9 @@
   <form role="search" method="get" class="d-flex" action="<?php echo esc_url(home_url('/')) ?>">
     <div class="input-group">
       <!-- Search input field -->
-      <input type="search" class="search-field form-control" id="nav-search" value="<?php echo get_search_query() ?>" name="s">
+      <input type="search" class="search-field form-control" value="<?php echo get_search_query() ?>" name="s" required>
       <!-- Search icon -->
-      <button type="submit" id="search-button" class="input-group-text">
+      <button type="submit" class="input-group-text">
         <svg class="bi">
           <use xlink:href="#fa-magnifying-glass" />
         </svg>
