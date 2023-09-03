a0da8bf60f461fc68d53a1cb4d175fa8b33448b7
Scott Killen <scott@scottkillen.com>
2023-09-03 17:04:03 -0400

    Change icon to submit button

--- template-parts/header/nav-search.php
+++ template-parts/header/nav-search.php
@@ -10,13 +10,13 @@
   <form role="search" method="get" class="d-flex" action="<?php echo esc_url(home_url('/')) ?>">
     <div class="input-group">
       <!-- Search input field -->
-      <input type="search" class="search-field form-control" id="search" value="<?php echo get_search_query() ?>" name="s">
+      <input type="search" class="search-field form-control" id="nav-search" value="<?php echo get_search_query() ?>" name="s">
       <!-- Search icon -->
-      <span class="input-group-text">
+      <button type="submit" id="search-button" class="input-group-text">
         <svg class="bi">
           <use xlink:href="#fa-magnifying-glass" />
         </svg>
-      </span>
+      </button>
     </div>
   </form>
 </div>
