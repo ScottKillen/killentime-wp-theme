18a09304ed7d96040ec65f400428449b058f6e04
Scott Killen <scott@scottkillen.com>
2023-09-12 21:35:51 -0400

    Fix branding font size

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -17,7 +17,7 @@ $logo_tag = (is_singular() || is_archive() || is_404() || is_search())
 ?>
 
 <div id="head" class="text-center pt-3 pb-5 site-branding">
-	<<?php echo $logo_tag ?> id="logo" class="site-logo text-center">
+	<<?php echo $logo_tag ?> id="logo" class="site-logo text-center display-4">
 		<img class="rounded-circle d-block overflow-hidden border bg-body-tertiary border-secondary mx-auto my-0 shadow-lg" src="<?php echo $logo_img_url; ?>" alt="Killentime logo" />
 		<span class="title site-title"><?php echo $blog_info; ?></span>
 		<span class="tagline text-body-secondary d-block mt-1 mb-2 font-accent fs-6 lh-sm"><?php echo $description; ?></span>
