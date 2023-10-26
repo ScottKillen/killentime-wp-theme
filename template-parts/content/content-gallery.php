90dab8b5ed96fabe89b75223e1dcf5063f230a2f
Scott Killen <scott@scottkillen.com>
2023-10-26 17:57:26 -0400

    Add gallery format

---
+++ template-parts/content/content-gallery.php
@@ -0,0 +1,37 @@
+<?php ?>
+<article <?php kt_post_id(); ?> <?php post_class('border-bottom border-secondary-subtle'); ?><?php semantics('post'); ?>>
+	<?php get_template_part(
+		'template-parts/entry/entry',
+		'header',
+		array(
+			'components' => array(
+				'post_date',
+				'post_category',
+			)
+		)
+	); ?>
+
+	<?php if (is_search()) : // Only display Excerpts for search pages
+	?>
+		<div class="entry-summary p-summary" itemprop="description">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	<?php else : ?>
+		<div class="py-3 entry-content e-content" itemprop="description">
+			<?php the_content('Continue reading <span class="meta-nav">&rarr;</span>'); ?>
+			<?php wp_link_pages(array('before' => '<div class="page-links"><span class="page-links-title">Pages:</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>')); ?>
+		</div><!-- .entry-content -->
+	<?php endif; ?>
+
+	<?php get_template_part(
+		'template-parts/entry/entry',
+		'footer',
+		array(
+			'components' => array(
+				'post_tag',
+				'comment_link',
+				'edit_link',
+			)
+		)
+	); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
