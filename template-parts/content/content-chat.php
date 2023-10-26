50ac71b266697e619cbb93a7b000e6362f769350
Scott Killen <scott@scottkillen.com>
2023-10-26 02:01:08 -0400

    Add chat format

---
+++ template-parts/content/content-chat.php
@@ -0,0 +1,41 @@
+<?php ?>
+<article <?php kt_post_id(); ?> <?php post_class(); ?><?php semantics('post'); ?>>
+	<?php get_template_part(
+		'template-parts/entry/entry',
+		'header',
+		array(
+			'components' => array(
+				'post_date',
+				'post_category',
+			)
+		)
+	); ?>
+
+	<?php if (is_search()) : // Only display Excerpts for Search
+	?>
+		<div class="entry-summary p-summary" itemprop="description">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+	<?php else : ?>
+		<div class="entry-content e-content" itemprop="articleBody description">
+			<div class="clearfix pb-3">
+				<?php kt_the_post_thumbnail('<p>', '</p>'); ?>
+			</div>
+			<?php the_content('Continue reading <span class="meta-nav">&rarr;</span>'); ?>
+			<?php wp_link_pages(array('before' => '<div class="page-link">Pages:', 'after' => '</div>')); ?>
+		</div><!-- .entry-content -->
+	<?php endif; ?>
+
+
+	<?php get_template_part(
+		'template-parts/entry/entry',
+		'footer',
+		array(
+			'components' => array(
+				'post_tag',
+				'comment_link',
+				'edit_link',
+			)
+		)
+	); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
