be059e3b425e417455e03bbb83ee2face8ce0303
Scott Killen <scott@scottkillen.com>
2023-10-26 17:41:58 -0400

    Fix page-formet

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -1,14 +1,4 @@
-<?php
-
-/**
- * Template part for displaying page content in page.php
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Scott_Killen
- */
-
-?>
+<?php ?>
 
 <article <?php kt_post_id(); ?> <?php post_class(); ?><?php semantics('page') ?>>
 	<?php get_template_part('template-parts/entry/entry', 'header'); ?>
@@ -19,10 +9,11 @@
 			<?php the_excerpt(); ?>
 		</div><!-- .entry-summary -->
 	<?php else : ?>
-		<?php kt_the_post_thumbnail('<div class="entry-media">', '</div>'); ?>
-
-		<div class="entry-content e-content" itemprop="description text">
-			<?php the_content(); ?>
+		<div class="clearfix py-3">
+			<div class="entry-content e-content" itemprop="description text">
+				<?php kt_the_post_thumbnail('<div class="entry-media">', '</div>'); ?>
+				<?php the_content(); ?>
+			</div>
 			<?php
 			wp_link_pages(
 				array(
@@ -30,23 +21,18 @@
 					'after'  => '</div>',
 				)
 			);
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__('Edit <span class="screen-reader-text">%s</span>', 'killentime'),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post(get_the_title())
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
 			?>
-		</div><!-- .entry-content -->
+		</div>
 	<?php endif; ?>
+
+	<?php get_template_part(
+		'template-parts/entry/entry',
+		'footer',
+		array(
+			'components' => array(
+				'edit_link',
+			)
+		)
+	); ?>
+
 </article><!-- #post-<?php the_ID(); ?> -->
