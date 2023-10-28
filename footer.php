882d06769d2d1c8e6ad5a65cd59f512264c17c8b
Scott Killen <scott@scottkillen.com>
2023-10-28 19:19:16 -0400

    Complete footer styling

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@ $logo_img_url = 'https://imagedelivery.net/0XfRl_9i2twIzyWa9HYA4g/ec98a6d7-bf85-
 	<div class="container">
 		<div class="d-flex flex-wrap justify-content-between align-items-center pt-2 mt-4 border-top">
 			<p class="col-md-4 mb-0 text-body-secondary font-accent">© <?php echo date('Y'); ?> Scott Killen. All rights reserved.</p>
-			<a href="<?php echo esc_url(home_url('/')) ?>" class="font-title col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-decoration-none link-body-emphasis">
+			<a href="<?php echo esc_url(home_url('/')) ?>" class="fs-4 font-title col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-decoration-none link-body-emphasis">
 				<?php bloginfo('name'); ?>
 			</a>
 			<ul class="nav col-md-4 justify-content-end">
