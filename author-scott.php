ef1d7c2c4185855aeb80f3243d1b0dbe0f0be700
Scott Killen <scott@scottkillen.com>
2023-10-29 01:04:11 -0400

    Fix social icons line-height

--- author-scott.php
+++ author-scott.php
@@ -11,7 +11,7 @@ get_header(); ?>
 				<div class="author-note note p-note" itemprop="description">
 					<p><span class="fn p-fn n p-name text-body-emphasis text-shadow">Scott Killen</span> is a multifaceted individual with a passion for both the spiritual and technological realms. By day, he's a <a class="p-name p-org u-url link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover" href="https://killencpa.com/" title="Killen & Associates, CPAs, PA">Certified Public Accountant</a>, managing numbers and ensuring financial order. On Sundays, he steps into the role of a teacher and preacher, sharing the wisdom and teachings of the Bible with <a class="p-name p-org u-url link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover" href="https://edistochurch.org/" title="Edisto Church of Christ">the congregation</a> though bible studies and sermons.</p>
 					<p>Beyond his professional life, he's a software developer, constantly diving into the ever-evolving world of technology. He also has a deep love for learning, always seeking out obscure knowledge to satisfy his curiosity. When not immersed in numbers or code, you'll find him enjoying quality time with his wife or pursuing his love for writing, including poetry. He enjoys taking nature photographs and has a love for carpentry, building and crafting with his own hands. Here, where faith, fun, and technology coexist, he writes about the things that interest him.</p>
-					<div>
+					<div class="social">
 						<p class=" pt-3 h3 fst-italic">Scott's other profiles:</p>
 						<ul class="col-md-6 list-unstyled">
 							<?php get_template_part(
