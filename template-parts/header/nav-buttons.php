14edb1e6c1fc74212fa78e05a48164bd55cb5835
Scott Killen <scott@scottkillen.com>
2023-11-06 00:45:27 -0500

    Make use of svg icons consistent

--- template-parts/header/nav-buttons.php
+++ template-parts/header/nav-buttons.php
@@ -25,7 +25,7 @@
   <li id="color-mode-switcher" class="nav-item dropdown">
     <button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (auto)">
       <svg class="bi my-1 theme-icon-active">
-        <use href="fa-circle-half-strokecircle-half"></use>
+        <use href="fa-circle-half-strokecircle-half" />
       </svg>
       <span class="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
     </button>
@@ -33,27 +33,27 @@
       <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
           <svg class="bi me-2 opacity-50 theme-icon">
-            <use href="#fa-sun-bright"></use>
+            <use href="#fa-sun-bright" />
           </svg>Light<svg class="bi ms-auto d-none">
-            <use href="#fa-check"></use>
+            <use href="#fa-check" />
           </svg>
         </button>
       </li>
       <li>
         <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
           <svg class="bi me-2 opacity-50 theme-icon">
-            <use href="#fa-moon-stars"></use>
+            <use href="#fa-moon-stars" />
           </svg>Dark<svg class="bi ms-auto d-none">
-            <use href="#fa-check"></use>
+            <use href="#fa-check" />
           </svg>
         </button>
       </li>
       <li>
         <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
           <svg class="bi me-2 opacity-50 theme-icon">
-            <use href="#fa-circle-half-stroke"></use>
+            <use href="#fa-circle-half-stroke" />
           </svg>Auto<svg class="bi ms-auto d-none">
-            <use href="#fa-check"></use>
+            <use href="#fa-check" />
           </svg>
         </button>
       </li>
